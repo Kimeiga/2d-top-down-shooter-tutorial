269f9dbd833ed133201693df9f98441265fd110b
Hakan Kimeiga <hak7alp@gmail.com>
2017-07-10 12:20:42 -0700

    yeah

---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour {
+
+	//Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+	public float speed;
+
+	public Rigidbody2D rigid;
+
+	Animator anim;
+
+	void Start() {
+		rigid = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update(){
+	
+		if (Input.GetMouseButtonDown (0)) {
+			anim.SetTrigger ("Attack");
+		
+		}
+
+	}
+
+	void FixedUpdate(){
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, 
+			Vector3.forward);
+
+		transform.rotation = rot;
+
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		rigid.angularVelocity = 0f;
+	
+
+		float input = Input.GetAxis ("Vertical");
+		rigid.AddForce (gameObject.transform.up * speed * input);
+
+
+	}
+}
---
+++ Assets/Scripts/SwordScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter2D(Collider2D other){
+	
+		Instantiate (explosion, other.transform.position, Quaternion.identity);
+
+		Destroy (other.gameObject);
+	}
+}
